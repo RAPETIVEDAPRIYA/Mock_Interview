cd7ca91fa97845cb1c3bf7e10a7e8424d7604a4f
Rapeti Veda Priya <vedapr471@gmail.com>
2025-06-18 09:35:37 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from voice import listen_command, speak
+from vision import ask_question
+
+def main():
+    while True:
+        command = listen_command()
+        if command:
+            speak("Processing your request.")
+            print("You said:", command)
+            try:
+                answer = ask_question(command)
+                print("Jarvis:", answer)
+                speak(answer)
+            except Exception as e:
+                print("Error:", e)
+                speak("Sorry, I could not get an answer.")
+
+if _name_ == "_main_":
+    main()
